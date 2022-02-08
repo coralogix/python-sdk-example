0dbfcb7370f5651ef0e6ace6051fb779557de328
juan-coralogix <89215136+juan-coralogix@users.noreply.github.com>
2022-02-08 13:32:46 -0300

    Updated email.

--- app.py
+++ app.py
@@ -2,25 +2,14 @@
 """
 Coralogix Python SDK Example
 Author: Coralogix Ltd.
-Email: juan@coralogix.com
-"""
-from coralogix.constants import Coralogix
-Coralogix.CORALOGIX_LOG_URL = 'https://api.coralogix.us:443/api/v1/logs'
-
-""" 
-
-CORALOGIX_LOG_URL Defines to which Coralogix Cluster will be sent the logs to:
-api.coralogix.us 
-api.coralogix.com (default)
-api.app.coralogix.in
-
+Email: support@coralogix.com
 """
 
 import logging
 from coralogix.handlers import CoralogixLogger
 
 
-PRIVATE_KEY = "03f2f7a3-0e6a-77e9-9072-XXXXXXXXXXXXX"
+PRIVATE_KEY = ""
 APP_NAME = "python"
 SUB_SYSTEM = "python"
 
